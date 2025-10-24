1eef92544e913b0bde6e4a73d7478cb43a556e87
nvd1402 <nguyenvandai89@gmail.com>
2025-10-24 09:52:31 +0700

    xong comment

--- wordpress/wp-content/themes/twentytwenty/sidebar-search_comment.php
+++ wordpress/wp-content/themes/twentytwenty/sidebar-search_comment.php
@@ -2,34 +2,65 @@
     <h2 class="sidebar-title">Bình luận mới nhất</h2>
 
     <?php
-    // Lấy 5 bình luận gần nhất
-    $recent_comments = get_comments(array(
+    // Lấy 5 comment cha
+    $parent_comments = get_comments(array(
         'number'      => 5,
         'status'      => 'approve',
         'post_status' => 'publish',
+        'parent'      => 0,
     ));
 
-    if ( $recent_comments ) :
-        foreach ( $recent_comments as $comment ) :
-            $author  = get_comment_author( $comment );
-            $content = wp_trim_words( $comment->comment_content, 30, '...' );
-            $link    = get_comment_link( $comment );
+    if ($parent_comments) :
+        foreach ($parent_comments as $comment) :
+            $author  = get_comment_author($comment);
+            $content = wp_trim_words($comment->comment_content, 40, '...');
+            $avatar  = get_avatar_url($comment, array('size' => 50));
             ?>
 
-            <div class="comment-item">
-                <div class="comment-avatar">
-                    <?php echo get_avatar( $comment, 48 ); ?>
+            <div class="media comment-box">
+                <div class="media-left">
+                    <a href="#">
+                        <img class="img-responsive user-photo" src="<?php echo esc_url($avatar); ?>" alt="<?php echo esc_attr($author); ?>">
+                    </a>
                 </div>
+                <div class="media-body">
+                    <h4 class="media-heading"><?php echo esc_html($author); ?></h4>
+                    <p><?php echo esc_html($content); ?></p>
 
-                <div class="comment-body">
-                    <h4 class="comment-author"><?php echo esc_html( $author ); ?></h4>
-                    <p class="comment-text"><?php echo esc_html( $content ); ?></p>
-                    <a href="<?php echo esc_url( $link ); ?>" class="comment-link">Xem chi tiết</a>
+                    <?php
+                    // Lấy comment con
+                    $child_comments = get_comments(array(
+                        'status' => 'approve',
+                        'parent' => $comment->comment_ID,
+                    ));
+                    if ($child_comments) :
+                        foreach ($child_comments as $child) :
+                            $child_author  = get_comment_author($child);
+                            $child_content = wp_trim_words($child->comment_content, 40, '...');
+                            $child_avatar  = get_avatar_url($child, array('size' => 50));
+                            ?>
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#">
+                                        <img class="img-responsive user-photo" src="<?php echo esc_url($child_avatar); ?>" alt="<?php echo esc_attr($child_author); ?>">
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading"><?php echo esc_html($child_author); ?></h4>
+                                    <p><?php echo esc_html($child_content); ?></p>
+                                </div>
+                            </div>
+                        <?php
+                        endforeach;
+                    endif;
+                    ?>
                 </div>
             </div>
 
-        <?php endforeach;
-    else : ?>
-        <p>Chưa có bình luận nào.</p>
-    <?php endif; ?>
+        <?php
+        endforeach;
+    else :
+        echo '<p>Chưa có bình luận nào.</p>';
+    endif;
+    ?>
 </aside>
